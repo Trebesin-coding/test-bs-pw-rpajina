9ad5868af57efa44d5bd3fed5e4443675459833f
rpajina <rosikova121@trebesin.cz>
2025-02-12 08:58:31 +0100

    done

--- polevka.py
+++ polevka.py
@@ -1,13 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 def main():
 
+    ingredients = []
 
-    # response = requests.get(url)
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    response = requests.get("https://js-trebesin.github.io/bsoup-exam/")
+
+    soup = BeautifulSoup(response.content, "html.parser") #<--- Úkol: popiš krátce, co tohle dělá : htmp.parser prelozi html tak, aby ho byla schopna soup zpracovat
+
+    ingrs = soup.find_all("h2")
+
+    for ingr in ingrs:
+        ingredients.append(ingr.text)
+
+    for i in range(4):
+        print(ingredients[i])
+            
+    
+
+    with open("recept.json", mode="w") as f:
+        json.dump(ingredients, f, indent=4, ensure_ascii=False)
+
+
+
+    
 
 
 if __name__ == "__main__":
--- secret.py
+++ secret.py
@@ -6,16 +6,30 @@ import os
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
 
-        # browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://js-trebesin.github.io/playwright-exam/")
+
+        page.fill('input[id="login"]', "Jarmil")
+        page.fill('input[id="pass"]', "Admin123")
 
         # !!!
         # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
         # !!!
 
+        page.click('button[class="login-btn"]')
+        page.wait_for_timeout(500)
 
-        # browser.close()
+
+        scrt_txt = page.get_by_text("42")
+        print(scrt_txt)
+
+        input("zavrit")
+
+        browser.close()
     
 
 if __name__ == "__main__":
